433f021e8cce817b6670d907cbce9c6a62c69d8f
Rebecca <rebecca.buchheit@gmx.de>
2020-01-26 17:36:20 +0100

    Homework

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
-
+const fs = require('fs');
 const app = express();
 
 app.engine('handlebars', handlebars());
@@ -8,19 +8,29 @@ app.set('view engine', 'handlebars');
 
 app.use(express.urlencoded({extended: true}));
 
-const messages = [];
+const messages = []; //Array
 
 app.get('/', (request, response) => {
     response.render('home', {messages: messages});
 });
 
 app.post('/new', (request, response) => {
+   if (fs.existsSync('./messages.json')) {
+    const text = fs.readFileSync('./messages.json', {encoding: 'utf-8'});
+    messages = JSON.parse(text);
+    }  else {
+    messages = [];
+    }
 
     messages.push({
         author: request.body.author,
         text: request.body.text,
         date: new Date()
     });
+
+    const text = JSON.stringify(messages);
+    fs.writeFileSync('./messages.json', text, {encoding: 'utf-8'});
+
     response.redirect('/');
 });
 
