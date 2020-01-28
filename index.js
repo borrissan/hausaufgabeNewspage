512d88925e4047432008ad4edb54de9834fefc55
David Linner <david@linner.berlin>
2020-01-28 11:32:45 +0100

    Added a possible solution for the second homework

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 
+const {getAllMessages, saveAllMessages, findMaxId } = require('./messages.js');
+
 const app = express();
 
 app.engine('handlebars', handlebars());
@@ -8,22 +10,40 @@ app.set('view engine', 'handlebars');
 
 app.use(express.urlencoded({extended: true}));
 
-const messages = [];
-
 app.get('/', (request, response) => {
+    const messages = getAllMessages();
     response.render('home', {messages: messages});
 });
 
 app.post('/new', (request, response) => {
+    const messages = getAllMessages();
+
+    const maxId = findMaxId(messages);
 
     messages.push({
         author: request.body.author,
         text: request.body.text,
-        date: new Date()
+        date: new Date(),
+        id: maxId + 1
     });
+
+    saveAllMessages(messages);
+
     response.redirect('/');
 });
 
+// html forms don't support method DELETE, so we unluckily have to take a POST on a path /delete
+app.post('/delete', (request, response) => {
+    let messages = getAllMessages();
+
+    const id = request.body.id;
+
+    messages = messages.filter(message => message.id != id);
+
+    saveAllMessages(messages);
+
+    response.redirect('/');
+});
 
 app.use(express.static('public'));
 
